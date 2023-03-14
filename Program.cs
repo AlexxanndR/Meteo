b357e0cc98b5efc004504705e44b4fd99cc97330
AlexxanndR <alexandrov.alexandder@gmail.com>
2023-03-14 22:36:42 +0300

    Made new fixes

--- Program.cs
+++ Program.cs
@@ -5,14 +5,16 @@ using System.IO.Ports;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using Xunit.Sdk;
 
 namespace Meteo
 {
     internal class Program
     {
-        private const string filePath = @".\meteo.json";
+        private const string filePath = @".\meteo3.json";
         private static List<MeteoInfo> meteoList = new List<MeteoInfo>();
         private static StringBuilder buffer = new StringBuilder();
+        private static int objectsToWriteCount = 0;
 
         public static void Main(string[] args)
         {
@@ -26,10 +28,7 @@ namespace Meteo
                 return;
             }
 
-            if (!File.Exists(filePath))
-                File.Create(filePath);
-
-            string? fileData = ReadFileData();
+            string? fileData = ReadDataFromFile();
             if (fileData == null)
                 return;
 
@@ -38,7 +37,7 @@ namespace Meteo
                 if (fileData.StartsWith("[") && fileData.EndsWith("]"))
                     meteoList = JsonSerializer.Deserialize<List<MeteoInfo>>(fileData);
             }
-            catch (Exception ex)
+            catch
             {
                 Console.WriteLine("The file contains incorrect data.");
                 return;
@@ -58,19 +57,29 @@ namespace Meteo
                 return;
             }
 
-            Console.WriteLine("The data is written to a file in the program directory.");
+            Console.WriteLine("The data file is located in the application directory.");
             Console.Write("Receiving messages from serial port...\nPress ESC to end... ");
             while (Console.ReadKey(true).Key != ConsoleKey.Escape) { }
+
+            if (objectsToWriteCount > 0)
+            {
+                string json = JsonSerializer.Serialize<List<MeteoInfo>>(meteoList.TakeLast(objectsToWriteCount).ToList(), new JsonSerializerOptions { WriteIndented = true });
+                WriteDataToFile(json);
+            }
 
             serialPort.Close();
         }
 
-        private static string? ReadFileData()
+        private static string? ReadDataFromFile()
         {
             string? fileData = null;
             try
             {
-                fileData = File.ReadAllText(filePath);
+                using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read))
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    fileData = sr.ReadToEnd();
+                };
             }
             catch (Exception ex)
             {
@@ -80,15 +89,47 @@ namespace Meteo
             return fileData;
         }
 
+        private static void WriteDataToFile(string data)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Write))
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    if (fs.Length > 0)
+                    {
+                        string resultData = ',' + data.Substring(1, data.Length - 1);
+                        fs.Seek(-3, SeekOrigin.End);
+                        sw.Write(resultData);
+                    }
+                    else
+                    {
+                        sw.Write(data);
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Writing to file error: {0}", ex.Message);
+            }
+        }
+
         private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
             string receivedData = ((SerialPort)sender).ReadExisting();
+
             buffer.Append(receivedData);
+            string bufferData = buffer.ToString();
+
+            if (!bufferData.StartsWith('$'))
+                buffer.Remove(0, bufferData.IndexOf('$') + 1);
 
             var messages = Regex.Matches(buffer.ToString(), @"\$\d+\.\d+,\d+\.\d+\r\n", RegexOptions.Compiled);
 
             foreach (Match message in messages)
             {
+                objectsToWriteCount++;
+
                 var numbers = Regex.Matches(message.Value, @"\d+\.\d+", RegexOptions.Compiled);
 
                 meteoList.Add(new MeteoInfo
@@ -99,12 +140,9 @@ namespace Meteo
                     WindDirection = float.Parse(numbers[1].Value, CultureInfo.InvariantCulture.NumberFormat)
                 });
 
-                string bufferData = buffer.ToString();
+                bufferData = buffer.ToString();
                 buffer.Remove(bufferData.IndexOf(message.Value), message.Value.Length);
             }
-
-            string json = JsonSerializer.Serialize<List<MeteoInfo>>(meteoList, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(filePath, json);
         }
     }
 }
